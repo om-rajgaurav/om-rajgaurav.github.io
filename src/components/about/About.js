ecdbaf90b8e37fa21db7a7e9a243bb55755780d1
Rohit Kumar <nnrohu@gmail.com>
2022-07-16 22:14:10 +0530

    fixed semicolon

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,7 +9,7 @@ import { VscFolderLibrary } from 'react-icons/vsc';
 function About() {
   return (
     <section id='about'>
-      <h5>Go To Know</h5>
+      <h5>Get To Know</h5>
       <h2>About Me</h2>
 
       <div className='container about__container'>
